fe815bf29d07e3610d28d450e3f4e1d0d4db95ac
zoechi <gzoechi@gmail.com>
2015-09-15 19:51:02 +0200

    added neon_animation example

---
+++ neon_animation/web/app_element.dart
@@ -0,0 +1,72 @@
+@HtmlImport('app_element.html')
+library neon_animation.app_element;
+
+import 'package:web_components/web_components.dart'
+    show CustomElementProxy, CustomElementProxyMixin, HtmlImport;
+import 'package:polymer/polymer.dart';
+import 'package:polymer_elements/neon_animatable_behavior.dart';
+import 'package:polymer_elements/neon_animation_runner_behavior.dart';
+import 'package:polymer_elements/paper_styles.dart';
+import 'package:polymer_elements/paper_input.dart';
+
+/// [PaperInput]
+@PolymerRegister('app-element')
+class AppElement extends PolymerElement
+    with
+        CustomElementProxyMixin,
+        PolymerBase,
+        NeonAnimatableBehavior,
+        NeonAnimationRunnerBehavior {
+  AppElement.created() : super.created();
+
+  @property String rotateFrom = '10';
+  @property String rotateTo = '100';
+
+  @eventHandler
+  void animateMe([_, __]) {
+    play();
+  }
+
+  @override
+  void attached() {
+    animationConfig = {
+      'value': () {
+        var squareNode = Polymer.dom(root).querySelector('.square');
+        return {
+          'entry': [
+            {
+              'name': 'transform-animation',
+              'transformFrom': 'scale(.9) translateY(100px)',
+              'transformTo': 'scale(1) translateY(0)',
+              'node': squareNode
+            },
+            {'name': 'fade-in-animation', 'node': squareNode}
+          ]
+        };
+      }
+    };
+  }
+
+  void ready() => play();
+
+  void play() {
+    updateAnimation();
+    playAnimation('entry', null);
+  }
+
+  void updateAnimation() {
+    var entryAnimation = this.animationConfig['entry'][0];
+
+    if (rotateFrom != null && rotateFrom != 0) {
+      entryAnimation['transformFrom'] = (entryAnimation['transformFrom']
+          as String).replaceFirst(new RegExp(r'rotate\(.*\)'), '');
+      entryAnimation['transformFrom'] += ' rotate(${rotateFrom}deg)';
+    }
+
+    if (rotateTo != null && rotateTo != 0) {
+      entryAnimation['transformTo'] = (entryAnimation['transformTo'] as String)
+          .replaceFirst(new RegExp(r'rotate\(.*\)'), '');
+      entryAnimation['transformTo'] += ' rotate(${rotateTo}deg)';
+    }
+  }
+}
---
+++ neon_animation/web/index.dart
@@ -0,0 +1,13 @@
+library neon_animation.app_element;
+
+//import 'dart:html';
+import 'package:polymer/polymer.dart';
+import 'app_element.dart';
+
+// source http://codepen.io/MWalid/pen/KpZpMz
+
+/// [AppElement]
+main() async {
+  await initPolymer();
+}
+
