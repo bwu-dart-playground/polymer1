c63ea4df37c699fb505d2247a082bb2198296658
zoechi <gzoechi@gmail.com>
2015-10-04 19:33:58 +0200

    upgrade to Polymer 1.0.0-rc.2

--- auto_binding_element/web/index.dart
+++ auto_binding_element/web/index.dart
@@ -1,37 +1,38 @@
-library auto_binding_element.main;
+//library auto_binding_element.main;
 
 import 'dart:html';
 import 'package:polymer/polymer.dart';
-import 'dart:js';
+//import 'dart:js';
 
 // TODO(zoechi) https://github.com/dart-lang/polymer-dart/issues/574
 
 //Map<String, dynamic> $;
-JsObject $;
+//JsObject $;
+DomBind template;
 main() async {
   await initPolymer();
-  var template = document.querySelector('#t') as DomBind;
+  template = document.querySelector('#t') as DomBind;
 
   template.on['dom-change'].listen((e) {
     print('dom-change');
   });
 
-  $ = template.$;
-  print('x');
-  var templateJs = template.jsElement;
+//  $ = template.$;
+//  var templateJs = template.jsElement;
   final model = new MyModel();
-  templateJs['model'] = jsValue(model);
+  template['model'] = model;
   // event handlers can't have `.` in declarative syntax and therefore
   // need to be assigned to the template.
-  templateJs['buttonTap'] = model.buttonTap;
+  template['buttonTap'] = model.buttonTap;
 }
 
-//@jsProxyReflectable
 class MyModel extends JsProxy {
+  @reflectable
   String value = 'something';
 
+  @reflectable
   buttonTap([Event e, args]) {
-    print($['mybutton'].id);
-    print('tap!');
+    print(template.$['mybutton'].id);
+    print('tap! $value');
   }
 }
